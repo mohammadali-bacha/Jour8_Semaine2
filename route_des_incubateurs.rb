909b3564ded2a77a09bdad24335d21277ebf1f7a
odssey <odssey@ya.ru>
2018-04-13 01:13:37 +0200

    dernière version updatée

--- route_des_incubateurs.rb
+++ route_des_incubateurs.rb
@@ -4,6 +4,14 @@ require 'open-uri'
 
 page = Nokogiri::HTML(open('http://www.penser-entrepreunariat.fr/?page_id=120'))
 
+
+#On récupère le code css dans l'inspecteur bouton droit dans
+#Elements de l'inspecteur sur la ligne qui nous intéresse
+#puis Copy > Copy element on le passe dans () de page.css
+#on a isolé les données qui nous intéresse il ne reste plus
+#qu'à les afficher
+#On a pas récupérer les codes postaux car pas présent sur
+# le site sélectionné voir url dans page /open
 page.css('#post-120 > ul').each do |el|
    puts el.text
 end
--- trader_de_l_obscur.rb
+++ trader_de_l_obscur.rb
@@ -1,26 +1,33 @@
 require 'rubygems'
 require 'nokogiri'
 require 'open-uri'
-
+#j'utilise http://ruby-doc.com/ pour récupérer les objets de Nokogiri
 def extract_currencies
   crypto_list = {}
   page = Nokogiri::HTML(open("https://coinmarketcap.com/all/views/all/"))
   crypto_list = {}
   elements = page.css('#currencies-all tbody tr')
-
+#j'utilise http://ruby-doc.com/ pour récupérer les objets de Nokogiri
+#on créé l'array curriences que l'on veut récupérer
   currencies = []
   elements.each do |element|
     currencies.push({
+      #dans l'array currencies les 2 seuls qui nous intéresse sont price et name
+      #pour les récupérer dans l'inspecteur >Network>Copy>Copy Elements
+      #on les passe dans les ()
       "name" => element.css('.currency-name-container').text,
       "price" => element.css('.price').text
     })
   end
-
+#on rappelle la currencies uniquement
   return currencies
 end
-
+#afin de relancer un rappel au serveur du site web
+#on repasse la function utilisée au tout début
+#on set en secondes ici 3600 mais on peut choisir
+#le script ne se fermera pas tant qu'il est allumé dans
+#le CLI / Faire CTR+C ou fermer la console au choix.
 while 1 do
   puts extract_currencies
   sleep 3600
 end
-
